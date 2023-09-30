b40ec88f303c14dbc94b604fea6794be7fda0b3b
Rustam-Asdanov <asdanovrustam@gmail.com>
2023-09-30 19:25:55 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { AppProvider } from './context/AppContext';
-import CartValue from './components/CartValue';
-import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
-import Location from './components/Location';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProvider } from "./context/AppContext";
+import CartValue from "./components/CartValue";
+import ExpenseList from "./components/ExpenseList";
+import ItemSelected from "./components/ItemSelected";
+import Location from "./components/Location";
 
 function App() {
   return (
-    <div className="App">
+    <AppProvider>
+      <div className="App">
         <h1>Shopping App</h1>
-        <div>
+        <div className="row mt-3">
+          <div className="col-sm">
             <CartValue />
+          </div>
+          <div className="col-sm">
             <Location />
+          </div>
         </div>
-        <h2>Shopping Cart</h2>
-        <div>
+
+        <h2 className="mt-3">Shopping Cart</h2>
+        <div className="row mt-3">
+          <div className="col-sm">
             <ExpenseList />
+          </div>
         </div>
-        <h2>Add Items</h2>
-        <div>
+
+        <h2 className="mt-3">Add Items</h2>
+        <div className="row mt-3">
+          <div className="col-sm">
             <ItemSelected />
+          </div>
         </div>
-    </div>
+      </div>
+    </AppProvider>
   );
 }
 
